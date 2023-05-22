cedc9992231c3a1d396d18c11d9c97bc7695237c
root <root@RichardT.localdomain>
2023-05-23 00:09:48 +0200

    added to create username and encrypt it

--- src/researchhub_comment/models.py
+++ src/researchhub_comment/models.py
@@ -1,34 +1,35 @@
 #flake8: noqa - importable modelsC
 from cryptography.fernet import Fernet
 from django.db import models
+import secrets
 import base64
 from .related_models.rh_comment_model import RhCommentModel
 from .related_models.rh_comment_thread_model import RhCommentThreadModel
 
 key = Fernet.generate_key()
-encoded_key = base64.urlsafe_b64encode(key).decode('utf-8')
+cipher_suite = Fernet(key)
 
 
 class Encrypted(RhCommentModel):
-    #encrypted_comment = models.TextField()
-    #encrypted_username = models.TextField()
+    cipher_suite = Fernet(key)
+    encryption_key = models.CharField(max_length=44)
 
     def encrypt(self, data):
-        cipher_suite = Fernet(key)
         encrypted_data = cipher_suite.encrypt(data.encode('utf-8'))
         return encrypted_data.decode('utf-8')
 
     def decrypt(self, encrypted_data):
-        cipher_suite = Fernet(key)
         decrypted_data = cipher_suite.decrypt(encrypted_data.encode('utf-8'))
         return decrypted_data.decode('utf-8')
 
     def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = secrets.token_hex(8)
+        if not self.encryption_key:
+            self.encryption_key = Fernet.generate_key().decode()
+
         encrypted_comment = self.encrypt(self.comment_content_json)
         self.comment_content_json = encrypted_comment
-        encrypted_user = self.encrypt(self.username)
-        self.username = encrypted_user
-        
         super().save(*args, **kwargs)
 
     class Meta:
